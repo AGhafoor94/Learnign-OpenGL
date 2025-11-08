3b7110757f5176add86254edad0a63c0443ab172
AGhafoor94 <adnan_ghafoor94@hotmail.com>
2025-11-08 13:44:04 +0000

    Add project files.

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <glad/glad.h>
+// GLFW (include after glad)
+// -mwindows
+#include <GLFW/glfw3.h>
+
+
+int main() {
+    glfwInit();
+    // need to tell GLFW hints at what version we are using
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 2 versions, core and core & compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* glfw_window = glfwCreateWindow(500, 500, "FIRST Project", NULL, NULL);
+    // error handling for window failing to generate
+    if (glfw_window == NULL) {
+        printf("Failed to create window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // now to tell glfw to use window
+    glfwMakeContextCurrent(glfw_window);
+
+    while (!glfwWindowShouldClose(glfw_window)) {
+        // poll messages
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(glfw_window);
+    glfwTerminate();
+
+    return 0;
+}
